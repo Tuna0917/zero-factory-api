a4d810ad2106e866117f4542628e510dee842de7
Tuna0917 <trick@kaist.ac.kr>
2025-09-19 19:39:53 +0900

    dto: member find or create

--- src/members/members.controller.ts
+++ src/members/members.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { MembersService } from './members.service';
 import { CreateMemberDto } from './dto/create-member.dto';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { MemberResponseDto } from './dto/member-response.dto';
 
 @ApiTags('members')
 @Controller('members')
@@ -13,8 +14,8 @@ export class MembersController {
     summary: '멤버 생성/조회',
     description: 'deviceId로 멤버를 찾거나 없으면 새로 생성합니다.',
   })
-  @ApiResponse({ status: 201, description: '멤버가 생성되거나 조회됨.' })
-  async findOrCreate(@Body() dto: CreateMemberDto) {
+  @ApiResponse({ status: 201, description: '멤버가 생성되거나 조회됨.', type: MemberResponseDto })
+  async findOrCreate(@Body() dto: CreateMemberDto): Promise<MemberResponseDto> {
     return this.membersService.findOrCreate(dto.deviceId);
   }
 }

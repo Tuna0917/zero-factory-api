73562ddd13b18f909b909283f30c25c1bffc4137
Tuna0917 <trick@kaist.ac.kr>
2025-09-19 19:39:24 +0900

    health-check

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { PlacesModule } from './places/places.module';
-import { MembersModule } from './members/members.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { PlacesModule } from '../places/places.module';
+import { MembersModule } from '../members/members.module';
+import { PrismaModule } from '../prisma/prisma.module';
 @Module({
   imports: [ConfigModule, PlacesModule, MembersModule, PrismaModule],
   controllers: [AppController],

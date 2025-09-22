a4d9df77fabae564535235e7b79fd78a71b65874
Tuna0917 <trick@kaist.ac.kr>
2025-09-22 15:34:34 +0900

    .

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET || 'dev-secret', // 환경변수에서 불러오기 권장
+      secretOrKey: process.env.JWT_SECRET,
     });
   }
 
